6d62b60c8f4d560e9231adb45394692d96c97e78
Deagle731 <deagleeeee01@gmail.com>
2021-09-23 10:01:57 +0400

    creeated Structure MVC

--- src/File_Model.php
+++ src/File_Model.php
@@ -5,67 +5,12 @@ class FileModel
 
     function View()
     {
-        echo "123VIEWSFILE";
+        echo "VIEW_FILE_MODEL";
     }
 
     function Create()
     {
-
-        $Errors = CheckDate1();
-
-
-        function CheckDate1()
-        {
-            $i = 0;
-            if ($_POST['Organization'] == '') {
-                $Errors['Organization'] = "<h5> Поле Организация обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Сounterparty'] == '') {
-                $Errors['Сounterparty'] = "<h5> Поле Контрагент обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-
-            if ($_POST['Signatory'] == '') {
-                $Errors['Signatory'] = "<h5> Поле Подписант обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Term-in'] == '') {
-                $Errors['Term-in'] = "<h5>Поле Срок договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Term-in'] == '') {
-                $Errors['Term-in'] = "<h5>Поле Срок договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Product'] == '') {
-                $Errors['Product'] = "<h5>Поле Предмет договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Amount'] == '') {
-                $Errors['Amount'] = "<h5>Поле Сумма договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Requisites'] == '') {
-                $Errors['Requisites'] = "<h5>Поле Реквизиты обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($i == 0) {
-                header('Location: /files/');
-                $result = json_encode($_POST);
-                $current = file_get_contents("date_files/documents/numberic");
-                $next = $current + 1;
-                file_put_contents("date_files/documents/{$next}", $result);
-                file_put_contents("date_files/documents/numberic", $next);
-            }
-            return $Errors;
-
-            print_r($_POST);
-        }
+        echo "CREATE_FILE_MODEL";
     }
 
 
@@ -76,90 +21,12 @@ class FileModel
 
     function Update()
     {
-        echo "123UPDATEFile";
-
-        function UpdateDate()
-        {
-            $i = 0;
-            if ($_POST['Organization'] == '') {
-                $Errors['Organization'] = "<h5> Поле Организация обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Сounterparty'] == '') {
-                $Errors['Сounterparty'] = "<h5> Поле Контрагент обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-
-            if ($_POST['Signatory'] == '') {
-                $Errors['Signatory'] = "<h5> Поле Подписант обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Term-in'] == '') {
-                $Errors['Term-in'] = "<h5>Поле Срок договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Term-in'] == '') {
-                $Errors['Term-in'] = "<h5>Поле Срок договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['Product'] == '') {
-                $Errors['Product'] = "<h5>Поле Предмет договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Amount'] == '') {
-                $Errors['Amount'] = "<h5>Поле Сумма договора обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($_POST['Requisites'] == '') {
-                $Errors['Requisites'] = "<h5>Поле Реквизиты обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($i == 0) {
-                header('Location: /files/');
-                $result = json_encode($_POST);
-                file_put_contents("date_files/documents/{$_GET['id']}", $result);
-            }
-            return $Errors;
-
-            print_r($_POST);
-        }
-
-        if (isset($_GET['id'])) {
-            $current_file  = $_GET['id'];
-            $currents = "date_files/documents/$current_file.json";
-            if (!isset($currents)) {
-                echo "User already delete";
-            } else {
-                $date = file_get_contents("date_files/documents/$current_file");
-                $json_array = json_decode($date, true);
-            }
-            if (isset($_POST['btn_editor'])) {
-
-                $Errors = UpdateDate();
-
-                header("Location:/files/");
-            }
-        }
+        echo "UPDATE_FILE_MODEL";
     }
 
 
     function Delete()
     {
-        if (isset($_GET['id'])){
-            $current_file  = $_GET['id'];
-             $current = file_get_contents("date_files/documents/$current_file");
-             if (!isset($current)){
-               echo "File deleted";
-             }
-             else{
-                 unlink("date_files/documents/$current_file");
-                 header('Location: /users/');
-             }
-          
-          }
+        echo "DELETE_FILE_MODEL"
     }
 }
--- src/User_Model.php
+++ src/User_Model.php
@@ -11,119 +11,21 @@ class UserModel
     function Create()
     {
         
-
-  
-        echo "CREATEUSERS";
-        function CheckDate1()
-        {
-            $i = 0;
-            if ($_POST['login'] == '') {
-                $Errors['login'] = "<h5> Поле login обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['firstname'] == '') {
-                $Errors['firstname'] = "<h5> Поле firstname обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-
-            if ($_POST['lastname'] == '') {
-                $Errors['lastname'] = "<h5> Поле lastname обязательно для заполнения </h5>";
-                $i += 1;
-            }
-
-            if ($_POST['birthday'] == '') {
-                $Errors['birthday'] = "<h5>Поле birthday обязательно для заполнения </h5>";
-                $i += 1;
-            }
-            if ($i == 0) {
-              //  header("Location:/users/");
-                $result = json_encode($_POST);
-                $current = file_get_contents("date_users/users/numberic");
-                $next = $current + 1;
-                file_put_contents("date_users/users/{$next}", $result);
-                file_put_contents("date_users/users/numberic", $next);
-            }
-            return $Errors;
-        }
-        $Errors = CheckDate1();
-
-        require_once("views/Create_users_view.php");
+        echo "CREATE_USER_MODEL";
+        
     }
 
     function Update()
     {
-        echo "123UPDATEUSERS";
-
-
-        function UpdateDate1()
-        {
-            $i = 0;
-            if ($_POST['login'] == '') {
-                $Errors1['login'] = "<h6> Поле login обязательно для заполнения </h6>";
-                $i += 1;
-            }
-
-            if ($_POST['firstname'] == '') {
-                $Errors1['firstname'] = "<h6> Поле firstname обязательно для заполнения </h6>";
-                $i += 1;
-            }
-
-
-            if ($_POST['lastname'] == '') {
-                $Errors1['lastname'] = "<h6> Поле lastname обязательно для заполнения </h6>";
-                $i += 1;
-            }
-
-            if ($_POST['birthday'] == '') {
-                $Errors1['birthday'] = "<h6>Поле birthday обязательно для заполнения </h6>";
-                $i += 1;
-            }
-            if ($i == 0) {
-                header("Location:/users/");
-                $result = json_encode($_POST);
-                file_put_contents("date_users/users/{$_GET['id']}", $result);
-            }
-            return $Errors1;
-        }
-
-
-
-        if (isset($_GET['id'])) {
-            $current_file  = $_GET['id'];
-            $currents = "date_users/users/$current_file.json";
-            if (!isset($currents)) {
-                echo "User already delete";
-            } else {
-                $date = file_get_contents("date_users/users/$current_file");
-                $json_array = json_decode($date, true);
-            }
-
-
-
-            if (isset($_POST['btn_editor'])) {
-                $Errors = UpdateDate1();
-                header("Location:/users/");
-            }
-        }
+        echo "UPDATE_USER_MODEL";
     }
 
 
 
     function Delete()
     {
-        echo "123DELETEUSERS";
+        echo "DELETE_USER_MODEL";
 
-        if (isset($_GET['id'])) {
-            $current_file  = $_GET['id'];
-            $current = file_get_contents("date_users/users/$current_file");
-            if (!isset($current)) {
-                echo "File deleted";
-            } else {
-                unlink("date_users/users/$current_file");
-                header('Location: /users/');
-            }
-        }
+    
     }
 }
